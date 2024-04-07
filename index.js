34b54b26517134e734dca4551d5135a1a5a695f6
Pranjal Rane <rane.pr@northeastern.edu>
2024-04-06 22:30:10 -0400

    add mongodb connection logic

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ import cors from "cors";
 import login from "./src/routes/login.js";
 import user from "./src/routes/user.js";
 import video from "./src/routes/video.js";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 var port = process.env.PORT || 4001;
+var MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI).then(() => {
+    //Success
+}).catch((err) => console.log(err));
 
 const app = express();
 app.use(cors());
