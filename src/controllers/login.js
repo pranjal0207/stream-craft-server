817803b5ba40e9b0a72572e12b20f84607365ae0
Pranjal Rane <rane.pr@northeastern.edu>
2024-04-08 00:30:34 -0400

    add API endpoint for user register and user fetch by ID

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,7 +1,43 @@
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
+
 export const login = async (req, res) => {
     res.status(200).json({"message" : "login"});
 }
 
 export const createNewUser = async (req, res) => {
-    res.status(200).json({"message" : "Create New User"});
+    try {
+        const {
+            username,
+            firstName,
+            lastName,
+            email,
+            password
+        } = req.body;
+
+        const newUserId = uuidv4();
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            user_id : newUserId,
+            username : username,
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            password: passwordHash,
+            subscriptions: [],
+            likedVideos : [],
+            dislikedVideos : [],
+            viewHistory : []
+        });
+
+        const saveUser = await newUser.save();
+        
+        res.status(200).json({"message" : saveUser});
+    } catch (err) {
+        res.status(500).json({"message" : err});
+    }
 }
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,14 @@
+import User from "../models/User.js";
+
 export const getUserById = async (req, res) => {
-    res.status(200).json({"message" : "Get User By ID"});
+    try {
+        const id = req.params.user_id;
+        const user = await User.findById(id);
+
+        res.status(200).json({"message": user});
+    } catch (error) {
+        res.status(500).json({"message": error});
+    }
 }
 
 export const updateEmailPassword = async (req, res) => {
