c3b80f58bf580f38cc2c4cf1ee356078b3598261
Pranjal Rane <rane.pr@northeastern.edu>
2024-04-09 11:14:14 -0400

    add API endpoint to update emial and password

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,9 +1,10 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 export const getUserById = async (req, res) => {
     try {
         const id = req.params.user_id;
-        const user = await User.findById(id);
+        const user = await User.findOne({user_id : id});
 
         res.status(200).json({"message": user});
     } catch (error) {
@@ -12,5 +13,13 @@ export const getUserById = async (req, res) => {
 }
 
 export const updateEmailPassword = async (req, res) => {
-    res.status(200).json({"message" : "Update Email Password"});
+    const id = req.params.user_id;
+    const { email, password } = req.body;   
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const updatedUser = await User.findOneAndUpdate({user_id : id}, {email: email, password: passwordHash}, { new: true }); 
+
+    res.status(200).json({"message" : updatedUser});
 }
