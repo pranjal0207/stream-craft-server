a9525b0bf39be8b69b594c87879e1cb6920cb765
Pranjal Rane <rane.pr@northeastern.edu>
2024-04-11 13:43:53 -0400

    add authentication support in videos API endpoints

--- src/routes/video.js
+++ src/routes/video.js
@@ -7,12 +7,14 @@ import {
   deleteVideo,
 } from "../controllers/video.js";
 import { handleUploadMiddleware } from "../services/s3/upload.js";
+import { verifyToken } from "../middleware/authentication.js";
+
 const router = express.Router();
 
-router.get("/getVideo/:videoId", getVideoByID);
-router.get("/getThumbnail/:videoId", getThumbnailById);
-router.post("/newVideo", handleUploadMiddleware, createNewVideo);
-router.put("/:videoId", updateTitleDescription);
-router.delete("/:videoId", deleteVideo);
+router.get("/getVideo/:videoId", verifyToken, getVideoByID);
+router.get("/getThumbnail/:videoId", verifyToken, getThumbnailById);
+router.post("/newVideo", verifyToken, handleUploadMiddleware, createNewVideo);
+router.put("/:videoId", verifyToken, updateTitleDescription);
+router.delete("/:videoId", verifyToken, deleteVideo);
 
 export default router;
