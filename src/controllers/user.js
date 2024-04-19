8a97e06c305edb726d1560826d15e58e58fe5c16
Pranjal Rane <rane.pr@northeastern.edu>
2024-04-19 16:33:34 -0400

    integration

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -19,13 +19,15 @@ export const updateEmailPassword = async (req, res) => {
     try {
         const {type, user_id}= req.params;
         const { email, password } = req.body;   
-    
+        console.log(email, password, user_id )
         const UserClass = type === "uploader" ? UploaderUser : ConsumerUser;
     
         const salt = await bcrypt.genSalt();
         const passwordHash = await bcrypt.hash(password, salt);
+        // console.log("in")
     
         const updatedUser = await UserClass.findOneAndUpdate({user_id : user_id}, {email: email, password: passwordHash}, { new: true }); 
+        console.log(updatedUser)
     
         res.status(200).json({"user" : updatedUser});
     } catch (error) {
